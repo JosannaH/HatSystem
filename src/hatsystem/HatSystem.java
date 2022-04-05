9a5a27c5b6f1dc1f46321b507f4ef61f6b40a395
fridaliljedahl <fridaliljedahl@hotmail.com>
2022-04-05 13:10:47 +0200

    test

--- src/hatsystem/HatSystem.java
+++ src/hatsystem/HatSystem.java
@@ -14,7 +14,7 @@ public class HatSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String s = "hej";
     }
     
 }
