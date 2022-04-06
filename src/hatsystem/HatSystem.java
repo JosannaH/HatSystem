8a6025481ea8f81b7a92dac4c604942f4e53db16
Josanna <josanna@live.se>
2022-04-06 10:02:05 +0200

    Inloggningssida

--- src/hatsystem/HatSystem.java
+++ src/hatsystem/HatSystem.java
@@ -16,7 +16,7 @@ public class HatSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+       new Login().setVisible(true);
     }
     /**
      * Test Jonas
