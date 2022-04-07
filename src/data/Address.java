620a833921d8716993cb44841d9aa962beef3a9c
fridaliljedahl <fridaliljedahl@hotmail.com>
2022-04-07 14:38:54 +0200

    Klar med spara kunduppgifter

--- src/data/Address.java
+++ src/data/Address.java
@@ -4,6 +4,8 @@
  */
 package data;
 
+import java.util.HashMap;
+
 /**
  *
  * @author Friday
@@ -20,11 +22,26 @@ public class Address {
      * @param city
      * @param country 
      */
-    public void addAdress(String street, String postal, String city, String country) {
+    public static void addAddress(String street, String postal, String city, String country) {
 
         String query = "INSERT INTO address (Street, Postal, City, Country) VALUES ('"+ street +"', '"+ postal +"', '"+ city +"', '"+ country +"');";
         SqlQuery.add(query);
     }
     
+    /**
+     * Retrieves an address from the database. Can be used to check if an address with the specified values already exists.
+     * @param street
+     * @param postal
+     * @param city
+     * @param country 
+     * @return A HashMap containing the address, empty if no address was found.
+     */
+    public static HashMap<String, String> getAddress(String street, String postal, String city, String country){
+        
+        String query = "SELECT * FROM address WHERE Street = '"+ street +"' AND Postal = '"+ postal +"' AND City = '"+ city +"' AND Country = '"+ country +"';";
+        HashMap<String, String> address = SqlQuery.getRow(query);
+        
+        return address;
+    }
     
 }
--- src/data/Customer.java
+++ src/data/Customer.java
@@ -4,7 +4,9 @@
  */
 package data;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 /**
  *
@@ -17,31 +19,81 @@ public class Customer {
 
     /**
      * Adds a customer to the database. User inputs should be checked before calling this method.
+     *
      * @param customerNr
      * @param firstName
      * @param lastName
      * @param email
      * @param phone
-     * @param adressID
-     * @param comment 
+     * @param addressID
+     * @param comment
      */
-    private void addCustomer(String customerNr, String firstName, String lastName, String email, String phone, String adressID, String comment) {
+    public static void addCustomer(String firstName, String lastName, String email, String phone, String addressID, String comment) {
 
-        String query = "INSERT INTO customer (Customer_Nr, First_Name, Last_Name, Email, Phone_Nr, Address, Comment) VALUES (" + customerNr + ", '" + firstName + "', '" + lastName + "', '" + email + "', '" + phone + "', " + adressID + ", '" + comment + "');";
-        SqlQuery.add(query);
+        boolean searching = true;
+        ArrayList<String> allNrs = getAllCustomerNrs();
+
+        while (searching) {
+
+            String customerNr = generateCustomerNr();
+
+            if (allNrs.contains(customerNr)) {
+                //do nothing and repeat
+            } else {
+                searching = false;
+                String query = "INSERT INTO customer (Customer_Nr, First_Name, Last_Name, Email, Phone_Nr, Address, Comment) VALUES (" + customerNr + ", '" + firstName + "', '" + lastName + "', '" + email + "', '" + phone + "', " + addressID + ", '" + comment + "');";
+                SqlQuery.add(query);
+            }
+        }
+    }
+
+    /**
+     * Used to generate a random customer number having 8 numbers.
+     *
+     * @return The generated customer number.
+     */
+    private static String generateCustomerNr() {
+
+        int x = 10000000 + new Random().nextInt(90000000);
+        String number = Integer.toString(x);
+        return number;
+    }
+
+    private static ArrayList<String> getAllCustomerNrs() {
+
+        ArrayList<String> allNrs = SqlQuery.getColumn("SELECT Customer_Nr FROM customer;");
+
+        return allNrs;
     }
 
     /**
      * Retrieves a customer from the database using a specified Customer_Nr.
+     *
      * @param customerNr
      * @return A HashMap containing the customer, empty if no customer having the specified id exits.
      */
-    private HashMap getCustomer(String customerNr){
-        
-        String query = "SELECT * FROM customer WHERE Customer_Nr = '"+ customerNr +"';";
+    public static HashMap<String, String> getCustomer(String customerNr) {
+
+        String query = "SELECT * FROM customer WHERE Customer_Nr = '" + customerNr + "';";
         HashMap<String, String> customer = SqlQuery.getRow(query);
-        
+
         return customer;
     }
-   
+
+    /**
+     * Retrieves a customer from the database using specified values. Can be used to check if a customer with entered details already exists.
+     *
+     * @param firstName
+     * @param lastName
+     * @param email
+     * @param phone
+     * @return A HashMap containing the customer, empty if no match was found.
+     */
+    public static HashMap<String, String> checkIfCustomerExists(String firstName, String lastName, String email, String phone) {
+        String query = "SELECT * FROM customer WHERE First_Name = '" + firstName + "' AND Last_Name = '" + lastName + "' AND Email = '" + email + "' AND Phone_Nr = '" + phone + "';";
+        HashMap<String, String> customer = SqlQuery.getRow(query);
+
+        return customer;
+
+    }
 }
