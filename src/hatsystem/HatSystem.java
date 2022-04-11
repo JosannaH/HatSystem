876c8ce945ece64a04d99a0158c890f835cb9bc4
Eveross0 <74544426+Eveross0@users.noreply.github.com>
2022-04-11 11:50:45 +0200

    Kortade ner en array list rad

--- src/hatsystem/HatSystem.java
+++ src/hatsystem/HatSystem.java
@@ -31,7 +31,7 @@ public class HatSystem {
 
         SqlQuery.setDatabase(idb);
 
-        new Login().setVisible(true);
+        //new Login().setVisible(true);
         new AddHatType().setVisible(true);
     }
 }
