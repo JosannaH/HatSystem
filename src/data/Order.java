45b38dd812e60f3234006da3f9ed970c6f2e5489
Josanna <josanna@live.se>
2022-04-22 06:49:08 +0200

    ny metod

--- src/data/Order.java
+++ src/data/Order.java
@@ -6,6 +6,7 @@ package data;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import javax.swing.DefaultListModel;
 
 /**
  *
@@ -16,7 +17,7 @@ public class Order {
     public static void addToOrderedHat(int hatID, int orderID) {
         String query = "INSERT INTO ordered_hat (Hat_ID, Order_Nr) VALUES ('" + hatID + "', '" + orderID + "');";
         SqlQuery.add(query);
-        
+
     }
 
     public static void addToOrderedStandardHat(String size, int hatID, int orderID) {
@@ -31,15 +32,74 @@ public class Order {
         return success;
     }
 
+    /**
+     * Get the most recent order
+     *
+     * @return
+     */
     public static int getOrderID() {
         String orderID = SqlQuery.getValue("SELECT MAX(Orders_ID) FROM Orders;");
         int orderIDInt = Integer.parseInt(orderID);
         return orderIDInt;
     }
 
+    /**
+     * Get all orders from database
+     *
+     * @return
+     */
     public static ArrayList<HashMap<String, String>> getAllOrders() {
         ArrayList<HashMap<String, String>> allOrders = SqlQuery.getMultipleRows("SELECT * FROM orders WHERE Active = 1;");
         return allOrders;
+    }
+
+    /**
+     * Get all orders with a specific status and list them in listmodel
+     *
+     * @param orderNr
+     * @param listModel
+     */
+    public static void listOrdersByStatus(String status, DefaultListModel listModel) {
+        listModel.clear();
+        ArrayList<HashMap<String, String>> orders = SqlQuery.getMultipleRows("SELECT * FROM orders WHERE Status = '" + status + "';");
+        int index = 0;
+        while (index < orders.size()) {
+            HashMap<String, String> currentOrder = orders.get(index);
+
+            listModel.addElement(String.format("%-10s %-20s %-20s %-20s"
+                    + currentOrder.get("Total_Price"),
+                    currentOrder.get("Orders_ID"),
+                    currentOrder.get("Delivery_Date"),
+                    currentOrder.get("Order_Date"),
+                    currentOrder.get("Status")
+            ));
+
+            index++;
+        }
+
+    }
+    
+     /**
+     * List all orders in a listmodel
+     */
+    public static void listAllOrders(DefaultListModel listModel) {
+        listModel.clear();
+
+        ArrayList<HashMap<String, String>> allOrders = Order.getAllOrders();
+        int index = 0;
+        while (index < allOrders.size()) {
+            HashMap<String, String> currentOrder = allOrders.get(index);
+
+            listModel.addElement(String.format("%-10s %-20s %-20s %-20s"
+                    + currentOrder.get("Total_Price"),
+                    currentOrder.get("Orders_ID"),
+                    currentOrder.get("Delivery_Date"),
+                    currentOrder.get("Order_Date"),
+                    currentOrder.get("Status")
+            ));
+
+            index++;
+        }
     }
 
 }
