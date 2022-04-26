6dce1fb646e9fed54beb9a196d8960b08ed04372
Eveross0 <74544426+Eveross0@users.noreply.github.com>
2022-04-26 15:41:54 +0200

    Okej nu

--- src/hatsystem/HatSystem.java
+++ src/hatsystem/HatSystem.java
@@ -31,8 +31,8 @@ public class HatSystem {
 
         SqlQuery.setDatabase(idb);
 
-        //new Login().setVisible(true);
-        new LoginMenu("Ottoadmin").setVisible(true);
+        new Login().setVisible(true);
+        
         
 
     }
